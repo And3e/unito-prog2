a0b57ec7efe9231bfb5cac9eb0a12a1adf0581c9
André <andre.marguerettaz@gmail.com>
2025-04-14 22:17:46 +0200

    implemented project 4a

--- c/10-progetto4a/charLinkedListQueueADT.c
+++ c/10-progetto4a/charLinkedListQueueADT.c
@@ -1,13 +1,107 @@
-#include "charQueueADT.h"
+#include "charArrayADT.h"
 
+/** @brief Restituisce una coda vuota, se non trova memoria restituisce NULL */
 CharQueueADT mkQueue() {
   CharQueueADT queue = malloc(sizeof(CharQueueADT));
 
   if (queue == NULL) {
     return NULL;
   }
-  
-  
+
+  queue->capacity = INITIAL_CAPACITY * pow(2, 0);
+  queue->a = malloc(queue->capacity);
+  queue->size = 0;
+  queue->front = 0;
+  queue->rear = 0;
 
   return queue;
+}
+
+/** @brief Distrugge la coda, recuperando la memoria */
+void dsQueue(CharQueueADT *pq) {
+  *pq = NULL;
+  free(pq);
+}
+
+/** @brief Aggiunge un elemento in fondo alla coda, restituisce esito 0/1 */
+_Bool enqueue(CharQueueADT q, const char e) {
+  if (q->size == q->capacity) {
+    int new_capacity = q->capacity * 2;
+
+    char *new_array = malloc(new_capacity);
+    if (new_array == NULL) {
+      return 0;
+    }
+
+    for (int i = 0; i < q->size; i++) {
+      new_array[i] = q->a[(q->front + i) % q->capacity];
+    }
+    free(q->a);
+
+    q->a = new_array;
+    q->capacity = new_capacity;
+    q->front = 0;
+    q->rear = q->size;
+  }
+
+  q->a[q->rear] = e;
+  q->rear = (q->rear + 1) % q->capacity;
+  q->size++;
+  return 1;
+}
+
+/** @brief Toglie e restituisce l'elemento in testa alla coda, restituisce esito
+ * 0/1 */
+_Bool dequeue(CharQueueADT q, char *res) {
+  if (q->size == 0) {
+    return 0;
+  }
+
+  *res = q->a[q->front];
+  q->front = (q->front + 1) % q->capacity;
+  q->size--;
+
+  if (q->size < (q->capacity / 4) && q->capacity > INITIAL_CAPACITY) {
+    int new_capacity = q->capacity / 2;
+
+    char *new_array = malloc(new_capacity);
+    if (new_array == NULL) {
+      return 0;
+    }
+
+    for (int i = 0; i < q->size; i++) {
+      new_array[i] = q->a[(q->front + i) % q->capacity];
+    }
+    free(q->a);
+
+    q->a = new_array;
+    q->capacity = new_capacity;
+    q->front = 0;
+    q->rear = q->size;
+  }
+
+  return 1;
+}
+
+/** @brief Controlla se la coda è vuota */
+_Bool isEmpty(CharQueueADT q) {
+  return !q->size;
+}
+
+/** @brief Restituisce il numero degli elementi presenti nella coda */
+int size(CharQueueADT q) {
+  return q->size;
+}
+
+/** @brief Restituisce l'elemento nella posizione data (a partire dalla testa
+ * con indice zero) (senza toglierlo), restituisce esito 0/1 */
+_Bool peek(CharQueueADT q, int position, char *res) {
+  if (position >= q->size) {
+    return 0;
+  }
+
+  int index = (q->front + position) % q->capacity;
+  *res = q->a[index];
+
+  return 1;
 }
--- c/10-progetto4a/charQueueADT.h
+++ c/10-progetto4a/charQueueADT.h
@@ -2,31 +2,33 @@
   #define _CHARQUEUEADT_
 
   #include <stdio.h>
+  #include <stdlib.h>
   #include <stdbool.h>
+  #include <math.h>
 
-  /* Un tipo di dato astratto per le code di char */
+  /** Un tipo di dato astratto per le code di char */
   typedef struct charQueue *CharQueueADT;
 
-  /* @brief Restituisce una coda vuota, se non trova memoria restituisce NULL */
+  /** @brief Restituisce una coda vuota, se non trova memoria restituisce NULL */
   CharQueueADT mkQueue();
 
-  /* @brief Distrugge la coda, recuperando la memoria */
+  /** @brief Distrugge la coda, recuperando la memoria */
   void dsQueue(CharQueueADT *pq);
 
-  /* @brief Aggiunge un elemento in fondo alla coda, restituisce esito 0/1 */
+  /** @brief Aggiunge un elemento in fondo alla coda, restituisce esito 0/1 */
   _Bool enqueue(CharQueueADT q, const char e);
 
-  /* @brief Toglie e restituisce l'elemento in testa alla coda, restituisce esito
+  /** @brief Toglie e restituisce l'elemento in testa alla coda, restituisce esito
   * 0/1 */
   _Bool dequeue(CharQueueADT q, char *res);
 
-  /* @brief Controlla se la coda è vuota */
+  /** @brief Controlla se la coda è vuota */
   _Bool isEmpty(CharQueueADT q);
 
-  /* @brief Restituisce il numero degli elementi presenti nella coda */
+  /** @brief Restituisce il numero degli elementi presenti nella coda */
   int size(CharQueueADT q);
 
-  /* @brief Restituisce l'elemento nella posizione data (a partire dalla testa con
+  /** @brief Restituisce l'elemento nella posizione data (a partire dalla testa con
   * indice zero) (senza toglierlo), restituisce esito 0/1 */
   _Bool peek(CharQueueADT q, int position, char *res);
 #endif
